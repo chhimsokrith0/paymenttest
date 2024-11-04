6392e10425e2b047b2e1c180555cd98caa314da4
chhimsokrith <sokrithchhim@gmail.com>
2024-11-04 18:36:28 +0700

    updateall

--- config/db.js
+++ config/db.js
@@ -7,6 +7,10 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true // This enables SSL and will use a secure connection
+  }
 });
 
 connection.connect((err) => {
